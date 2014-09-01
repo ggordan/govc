7b85b722da03bc3524c157c766115c821ca31af2
Gordan Grasarevic <me@ggordan.com>
2014-09-01 23:26:56 +0100

    show checkout on hover

--- js/pages/repo/branches/branch.js
+++ js/pages/repo/branches/branch.js
@@ -10,10 +10,16 @@ var Branch = React.createClass({
     // RENDER
 
     render: function() {
+
+        var head = (this.props.data.Head) ? <span className="head">(head)</span> : null;
+
         return(
             <li>
             <span className="icon icon-flow-branch" />
-            <span className="title"> {this.props.data.Name} </span>
+            <div className="title">
+                <span>{this.props.data.Name}</span>
+                {head}
+            </div>
             </li>
         );
     },
